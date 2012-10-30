45d02c40c4a01c27e09ac34a0fc12c5675d6680a
rossfadely <rossfadely@gmail.com>
2012-10-30 13:28:19 -0400

    adding some code

---
+++ code/nn_patchmodel.py
@@ -0,0 +1,84 @@
+import numpy as np
+
+class PsfModels(object):
+    """
+    Create a set of psf patches, drawing
+    from a power law in
+    'score' = (flux)**2/(fwhm)**2/sigma_const**2 
+    """
+    def __init__(self,psfhwhm=1.5,
+                 patchshape=(15,15),
+                 npatches=10,
+                 scorerange=(50,1000),
+                 flux=1000,
+                 bkg=0):
+        self.psfhwhm = psfhwhm
+        self.patchshape = patchshape
+        self.npatches = npatches
+        self.scorerange = scorerange
+        self.flux = flux
+        self.bkg = bkg
+
+        assert (np.mod(patchshape,2) & np.mod(patchshape,2)),
+            'Patch size must be odd by odd'
+        
+        # PSF stars in center of patch
+        self.psfpos = ((patchshape[0]-1)/2,
+            (patchshape[1]-1)/2)
+
+        self.make_patches()
+
+    def gaussianpsf(self,flux,xgrid,ygrid,x0,y0,psfhwhm):
+        """
+        Return a gaussian psf on grid of pixels
+        """
+        psf = flux * np.exp(-0.5 * ((xgrid-x0) ** 2 + (ygrid-y0) ** 2)
+                            / psfhwhm ** 2) / np.sqrt(2. * np.pi * psfhwhm ** 2)
+        return psf
+
+    def make_patch(self,bkg_sigma):
+        """
+        Make a patch of pixels with constant noise and gaussian psf
+        """
+        patch = np.random.normal(size=model.psfshape) * bkg_sigma
+        patch += model.bkg
+        nx,ny = model.psfshape
+        x,y   = np.meshgrid(range(nx),range(ny))
+        patch += gaussianpsf(model.flux,x,y,model.psfpos[0],
+                             model.psfpos[1],model.psfhwhm)
+        return patch
+
+    def draw_score(self):
+        """
+        Draw score from a power law... how
+        did I define that again?
+        """
+        scoremin = self.scorerange[0]
+        scoremax = self.scorerange[1]
+        scorerng = scoremax-scoremin
+        noscore = True
+        while noscore:
+            score = np.random.rand() * \
+                (scorerng) + scoremin
+            cdf   = scoremax * (score-scoremin) / \
+                score / (scorerng)
+            if 1-cdf > np.random.rand():
+                  noscore = False
+        return score
+
+    def make_patches(self):
+        """
+        Make the patches
+        """
+        self.patches = np.zeros((npatches,patchshape[0],
+                                 patchshape[1]))
+        self.scores = np.zeros(npatches)
+        self.bkg_sigmas = np.zeros(npatches)
+
+        for i in range(self.npatches):
+            self.scores[i] = self.draw_score()
+            self.bkg_sigmas[i] = np.sqrt(self.flux**2. / (2*self.psfhwhm)**2. / \
+                                         self.score[i])
+            self.patches[i] = self.make_patch(self.bkg_sigmas[i])
+
+
---
+++ code/nn_utils.py
@@ -0,0 +1,45 @@
+import scipy.ndimage as ndimage
+
+from scipy.optimize import fmin_l_bfgs_b
+from scipy.optimize import fmin
+
+
+def sq_nearest(values):
+    return np.sum((values-values[len(values)/2])**2.)
+
+def lnlike(modelpatch,data,sig_smooth,sig_L2,sig_one,
+                 w_L2):
+    """
+    Return the negative log-likelihood given a pixel patch across a
+    given set of data patches, weighted by regularization priors.
+    Uniform noise case.
+    """
+    
+    # Likelihood given current psf model
+    lnlike = 0.0
+    for ii in range(data.npatches):
+        patch = np.ravel(data.patches[ii])
+        flux  = np.dot(modelpatch.T,patch)/np.dot(modelpatch.T,modelpatch)
+        model = modelpatch*flux
+        lnlike += np.sum(0.5*(patch-model) ** 2
+                         / data.bkg_sigmas[ii]**2. + \
+                         0.5 * np.log(data.bkg_sigmas[ii]**2.))
+
+    # Smoothness constraint
+    if sig_smooth!=0:
+            filt = np.array([[False,True,False],
+                             [True,True,True],
+                             [False,True,False]])
+            nearest = ndimage.generic_filter(np.reshape(modelpatch,data.patchshape),
+                                     sq_nearest, footprint=filt)
+            lnlike  += np.sum(nearest) * sig_smooth
+
+    # L2 norm
+    if sig_L2!=0:
+        lnlike += np.sum((modelpatch*w_L2)**2.) * sig_L2
+
+    # PSF total ~ 1
+    if sig_one!=0:
+        lnlike += (np.sum(modelpatch)-1)**2. * sig_one
+
+    return lnlike
